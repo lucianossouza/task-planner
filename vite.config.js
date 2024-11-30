df034957043410fd5aa99c1bb5f7b06cb78fe6d6
lucianossouza <luciano.illustrador@hotmail.com>
2024-11-30 19:19:51 -0300

    configuracao do deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ghPages({
+      branch: "gh-pages",
+      silent: false,
+    }),
+  ],
+  base: "/task-planner",
+  build: {
+    rollupOptions: {
+      input: "/index.html",
+    },
+  },
+});
