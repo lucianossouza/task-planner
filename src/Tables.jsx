3be4bdca9fd119070a2b27c810deababfe06cb8a
lucianossouza <luciano.illustrador@hotmail.com>
2024-11-30 18:20:29 -0300

    criacao das tabelas

--- src/Tables.jsx
+++ src/Tables.jsx
@@ -1,55 +1,31 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Stack } from "@mui/material";
+import Button from "./Button";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
-function createData(name, calories, fat, carbs, protein, price) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      {
-        date: '2020-01-05',
-        timeStart: '11H-09M',
-        timeEnd: '11H-09M',
-        amount: 3,
-      },
-      {
-        date: '2020-01-02',
-        timeStart: '11H-09M',
-        timeEnd: '11H-09M',
-        amount: 1,
-      },
-    ],
-  };
-}
-
-function Row(props) {
-  const { row } = props;
+function Row({ handleOpen, row }) {
   const [open, setOpen] = React.useState(false);
 
   return (
     <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
         <TableCell>
           <IconButton
             aria-label="expand row"
             size="small"
+            fjusty
             onClick={() => setOpen(!open)}
           >
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
@@ -58,44 +34,74 @@ function Row(props) {
         <TableCell component="th" scope="row">
           {row.name}
         </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
+        <TableCell align="center">{row.workingHours}</TableCell>
+        <TableCell align="right">
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(row.totalAmount)}
+        </TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                Nome da Tarefa
-              </Typography>
               <Table size="small" aria-label="purchases">
                 <TableHead>
                   <TableRow>
                     <TableCell>Data</TableCell>
-                    <TableCell>Hora entrada</TableCell>
-                    <TableCell>Hora saída</TableCell>
-                    <TableCell align="right">Subtração entrada e saída</TableCell>
+                    <TableCell align="center">Hora entrada</TableCell>
+                    <TableCell align="center">Hora saída</TableCell>
+                    <TableCell align="center">
+                      Subtração entrada e saída
+                    </TableCell>
                     <TableCell align="right">Valor total</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.timeStart}</TableCell>
-                      <TableCell>{historyRow.timeEnd}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
+                  {row.history.map((historyRow) => {
+                    if (historyRow?.date) {
+                      return (
+                        <TableRow key={historyRow.date}>
+                          <TableCell component="th" scope="row">
+                            {
+                              new Date(`${historyRow.date}T00:00:00`)
+                                .toLocaleString("pt-BR")
+                                .split(",")[0]
+                            }
+                          </TableCell>
+                          <TableCell align="center">
+                            {historyRow.timeStart}
+                          </TableCell>
+                          <TableCell align="center">
+                            {historyRow.timeEnd}
+                          </TableCell>
+                          <TableCell align="center">
+                            {historyRow.workingHours}
+                          </TableCell>
+                          <TableCell align="right">
+                            {new Intl.NumberFormat("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                            }).format(historyRow.totalAmount)}
+                          </TableCell>
+                        </TableRow>
+                      );
+                    }
+                  })}
                 </TableBody>
               </Table>
+              <Stack
+                marginTop={"12px"}
+                flexDirection={"row"}
+                justifyContent={"flex-end"}
+              >
+                <Button
+                  onClick={() => {
+                    handleOpen(row.id);
+                  }}
+                />
+              </Stack>
             </Box>
           </Collapse>
         </TableCell>
@@ -105,52 +111,63 @@ function Row(props) {
 }
 
 Row.propTypes = {
+  handleOpen: PropTypes.func.isRequired,
   row: PropTypes.shape({
-    calories: PropTypes.number.isRequired,
-    carbs: PropTypes.number.isRequired,
-    fat: PropTypes.number.isRequired,
+    id: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    workingHours: PropTypes.string.isRequired,
+    totalAmount: PropTypes.number.isRequired,
     history: PropTypes.arrayOf(
       PropTypes.shape({
-        amount: PropTypes.number.isRequired,
+        workingHours: PropTypes.string.isRequired,
+        totalAmount: PropTypes.number.isRequired,
         timeStart: PropTypes.string.isRequired,
         timeEnd: PropTypes.string.isRequired,
         date: PropTypes.string.isRequired,
-      }),
+      })
     ).isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    protein: PropTypes.number.isRequired,
   }).isRequired,
 };
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
-
-export default function CollapsibleTable() {
+export default function CollapsibleTable({ data, handleOpen }) {
   return (
     <TableContainer component={Paper}>
       <Table aria-label="collapsible table">
         <TableHead>
           <TableRow>
-            <TableCell />
+            <TableCell width={"50px"} />
             <TableCell>Nome da tarefa</TableCell>
-            <TableCell align="right">Horas entrada</TableCell>
-            <TableCell align="right">Horas saída</TableCell>
-            <TableCell align="right">Subtração entrada e saída</TableCell>
+            <TableCell align="center">Horas trabalhadas</TableCell>
             <TableCell align="right">Valor total</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <Row key={row.name} row={row} />
+          {data.map((row) => (
+            <Row key={row.name} row={row} handleOpen={handleOpen} />
           ))}
         </TableBody>
       </Table>
     </TableContainer>
   );
 }
+
+CollapsibleTable.propTypes = {
+  handleOpen: PropTypes.func.isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      workingHours: PropTypes.string.isRequired,
+      totalAmount: PropTypes.number.isRequired,
+      history: PropTypes.arrayOf(
+        PropTypes.shape({
+          workingHours: PropTypes.string.isRequired,
+          totalAmount: PropTypes.number.isRequired,
+          timeStart: PropTypes.string.isRequired,
+          timeEnd: PropTypes.string.isRequired,
+          date: PropTypes.string.isRequired,
+        })
+      ),
+    })
+  ).isRequired,
+};
