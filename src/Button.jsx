a05595cde40e36c6261d88330d993a2ae121f621
lucianossouza <luciano.illustrador@hotmail.com>
2024-11-30 18:18:34 -0300

    adicionado um botao generico

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import Stack from '@mui/material/Stack';
-import BasicModal from './Modal';
+import { Button as MUIButton } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import PropTypes from "prop-types";
 
-export default function IconLabelButtons() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  
+export default function Button({ onClick, label }) {
   return (
-    <Stack direction="row" spacing={2} justifyContent={"flex-end"} width={"100%"}>
-      <Button onClick={handleOpen} variant="outlined" startIcon={<AddCircleIcon />}>
-        Criar registro
-      </Button>
-      <BasicModal open={open} handleClose={handleClose}/>
-    </Stack>
+    <MUIButton
+      sx={{ gap: "12px" }}
+      onClick={onClick}
+      variant={label ? "outlined" : "text"}
+    >
+      <AddCircleIcon />
+      {label}
+    </MUIButton>
   );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  label: PropTypes.string,
+};
