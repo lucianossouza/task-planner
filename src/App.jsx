1e96155326b6f9d1d102f320e497ec4cd97e54c9
lucianossouza <luciano.illustrador@hotmail.com>
2024-11-30 18:13:17 -0300

    implantacao inicial da aplicacao task planner

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,75 @@
-import React from "react";
-import IconLabelButtons from "./Button";
+import Button from "./Button";
 import CollapsibleTable from "./Tables";
+import BasicModal from "./Modal";
+import { useEffect, useState } from "react";
+import Stack from "@mui/material/Stack";
 
 const App = () => {
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState([]);
+  const [registryID, setRegistryID] = useState();
+
+  const handleOpen = (id) => {
+    setOpen(true);
+    setRegistryID(id);
+  };
+  const handleClose = () => setOpen(false);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        "https://674a75678680202966348afe.mockapi.io/task-planner/registry",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Erro: ${response.status} - ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error("Erro:", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <header className="header">
         <h1>Registro de Horas</h1>
       </header>
       <main>
-        <div className="container">
-          <IconLabelButtons />
-        </div>
+        <Stack
+          direction="row"
+          spacing={2}
+          justifyContent={"flex-end"}
+          width={"100%"}
+        >
+          <div className="container">
+            <Button
+              onClick={() => handleOpen(undefined)}
+              label={"Criar registro"}
+            />
+          </div>
+        </Stack>
         <section>
-          <CollapsibleTable />
+          <CollapsibleTable data={data} handleOpen={handleOpen} />
         </section>
+        <BasicModal
+          open={open}
+          handleClose={handleClose}
+          getData={getData}
+          registryID={registryID}
+        />
       </main>
     </>
   );
